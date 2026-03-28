c0b3886aa79e12679c35b2ccbb5479dd0d024acf
Rahmaatta <Rahma.mohamed.22230340@commerce.helwan.edu.eg>
2026-03-28 12:09:42 +0200

    Question02

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -16,3 +16,33 @@
 
 // e) A method receives a Logger as a parameter and calls it inside the method only.
 // Dependency (temporary usage)
+
+// =======================
+// Q2: Access Modifiers & Sealed
+// =======================
+
+// a) Protected field:
+// - Child class in different assembly can access it? yes (through inheritance only)
+// - Can it be accessed through an object instance from outside? NO
+
+// b) Difference:
+// protected internal  >> accessible in same assembly OR derived class in another assembly
+// private protected   >> accessible in same assembly AND only in derived classes
+
+// c) sealed keyword:
+// - On a class: prevents inheritance (cannot be a base class)
+// - On a method: prevents further overriding
+
+// d) Can we create an object from a sealed class?
+// yes, because sealed only prevents inheritance, not object creation
+
+// Example:
+//sealed class Car { }
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Car c = new Car(); // valid
+//    }
+//}
